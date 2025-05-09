f373b7ab9839b6ef20f31179e61c62ec260e4fef
mohamed hegazy <hheegg9999@gmail.com>
2025-05-09 19:09:29 +0300

    add reset PAssward

--- src/modules/Shared/AuthLayout/AuthLayouts.tsx
+++ src/modules/Shared/AuthLayout/AuthLayouts.tsx
@@ -1,8 +1,51 @@
  import React from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
+import Logo from '../../../assets/Logo-white.png'
+import authImage from '../../../assets/authImage.svg'
  
- const AuthLayouts = () => {
+const AuthLayouts = () => {
+  const location = useLocation();
+  const getPageTitle = () => {
+    switch (location.pathname) {
+      case '/login':
+        return 'Login';
+      case '/register':
+        return 'Register';
+      case '/forget-password':
+        return 'Forgot Password';
+      case '/reset-password':
+        return 'Reset Password';
+      case '/change-password':
+        return 'Change Password';
+      default:
+        return 'Login';
+    }
+  };
    return (
-     <div>AuthLayouts </div>
+     <>
+       <div className="min-h-screen grid grid-cols-1 lg:grid-cols-5 bg-black ">
+        {/* Left Section */}
+
+        <div className="auth-title p-12 lg:col-span-3 custom:col-span-12">
+           <img src={Logo} alt="Logo" className="w-48 pb-12 " />
+           
+           <h1 className="text-4xl font-bold pb-12">
+           {getPageTitle()}
+           </h1>
+
+          <Outlet />
+        </div>
+
+        {/* Right Section */}
+        <div className="p-6 lg:col-span-2 custom:col-span-12 px-11 sm:px-6 ">
+          <img
+            src={authImage}
+            alt="Quiz app Illustration"
+            className="w-full h-full object-contain"
+          />
+        </div>
+      </div>
+     </>
    )
  }
  
