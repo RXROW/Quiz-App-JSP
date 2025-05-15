2abad70c37562bce390771524031ac303f851acc
IslamAmer2211 <islamamer2211@gmail.com>
2025-05-15 16:50:30 +0300

    Pagination/AddButton

--- src/Store/Authantication/AuthSlice.ts
+++ src/Store/Authantication/AuthSlice.ts
@@ -4,7 +4,8 @@ import { AuthApiSlice } from "./AuthApi";
 
 const initialState = {
   token: localStorage.getItem("token") || null,
-  user: null,
+  // user: null,
+  user: JSON.parse(localStorage.getItem("user") || "null"),
 };
 
 const authSlice = createSlice({
@@ -15,15 +16,22 @@ const authSlice = createSlice({
       state.token = null;
       state.user = null;
       localStorage.removeItem("token");
+      localStorage.removeItem("user");
     },
   },
   extraReducers: (builder) => {
     builder.addMatcher(
       AuthApiSlice.endpoints.login.matchFulfilled,
       (state, action) => {
-        state.token = action.payload.data.accessToken;
-        localStorage.setItem("token", action.payload.data.accessToken);
-        state.user = action.payload.data.user;
+        // state.token = action.payload.data.accessToken;
+        // localStorage.setItem("token", action.payload.data.accessToken);
+        // state.user = action.payload.data.user;
+        const { accessToken, profile } = action.payload.data;
+        state.token = accessToken;
+        state.user = profile;
+
+        localStorage.setItem("token", accessToken);
+        localStorage.setItem("user", JSON.stringify(profile));
       }
     );
   },
---
+++ src/modules/Shared/AddButton/AddButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface AddButtonProps {
+  onClick: () => void;
+  text?: string;
+}
+
+export default function AddButton({ onClick, text  }: AddButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="flex items-center gap-2 px-4 py-2 border border-black rounded-full text-sm hover:bg-black hover:text-white transition"
+    >
+      <i className="fas fa-plus-circle text-lg"></i> {text}
+    </button>
+  );
+}
