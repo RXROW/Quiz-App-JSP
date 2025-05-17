4dbbfeb2c3034fbd21c3ee1281d952e0bbb9ea72
mohamed hegazy <hheegg9999@gmail.com>
2025-05-17 21:54:44 +0300

    editConfig Api

--- src/services/apis/apisConfig.ts
+++ src/services/apis/apisConfig.ts
@@ -14,11 +14,9 @@ export const privateInstance: AxiosInstance = axios.create({
 
 // Add an interceptor to the private instance to automatically include the Authorization header
 privateInstance.interceptors.request.use((config:any) => {
-  const atoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2Njc4NjNhNWM4NWYxZWNkYmMyNjk1NzUiLCJlbWFpbCI6InVwc2tpbGxpbmcubW9uaXRvcnNAZ21haWwuY29tIiwicm9sZSI6Ikluc3RydWN0b3IiLCJpYXQiOjE3NDczODgzODIsImV4cCI6MTc1MDk4ODM4Mn0.MWDxqdvmct1G10Dv2oIGk4PAiDiXHclHw83S65ZrU0U"
-
-  // const token = localStorage.getItem(atoken)
-  if (atoken) {
-    config.headers.Authorization = `Bearer ${atoken}`
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
   }
   return config
 })
